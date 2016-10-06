61a30ae5154158d494dd4b65d532e37b0f76bac5
Alexis Phuong <alexis.hvp@gmail.com>
2016-10-06 14:35:31 +0200

    ajout de classe css

--- js/map.js
+++ js/map.js
@@ -1,3 +1,10 @@
 function initialisation(){
-    
+    function initialisation(){
+        var optionsCarte = {
+            zoom: 8,
+            center: new google.maps.LatLng(47.389982, 0.688877)
+        }
+        var maCarte = new google.maps.Map(document.getElementById("EmplacementDeMaCarte"), optionsCarte);
+    }
+    google.maps.event.addDomListener(window, 'load', initialisation);
 }
