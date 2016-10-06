9e137abc775b082de4d9165e382f530b2d49e2a8
Alexis Phuong <alexis.hvp@gmail.com>
2016-10-06 15:15:21 +0200

    ajout d'un curseur sur la map

--- js/map.js
+++ js/map.js
@@ -1,10 +1,31 @@
+/*
 function initialisation(){
-    function initialisation(){
         var optionsCarte = {
-            zoom: 8,
-            center: new google.maps.LatLng(47.389982, 0.688877)
-        }
-        var maCarte = new google.maps.Map(document.getElementById("EmplacementDeMaCarte"), optionsCarte);
+            zoom: 4,
+            center: new google.maps.LatLng(-33, 151)
+            // Remarque : Il est désormais possible de remplacer :
+            // center: new google.maps.LatLng(47.389982, 0.688877)
+            // par :
+            // center: {lat: 47.389982, lng: 0.688877}
+        };
+        var myMap = new google.maps.Map(document.getElementById("zootopie"), optionsCarte);
     }
-    google.maps.event.addDomListener(window, 'load', initialisation);
+
+  */
+
+    // ajout d'un méchant sur la map
+
+
+function initialisation() {
+    var map = new google.maps.Map(document.getElementById('zootopie'), {
+        zoom: 4,
+        center: {lat: 48.8589507, lng: 2.2775163}
+    });
+
+    var image = 'img/icon.png';
+    var beachMarker = new google.maps.Marker({
+        position: {lat: 48.876905, lng: 2.270650},
+        map: map,
+        icon: image
+    });
 }
---
+++ js/personnage.js
@@ -0,0 +1,5 @@
+var personnage = {
+    race:
+    age:
+    
+};
